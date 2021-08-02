5214e88e508dcbd55192fd2d5a0bb2fe6b86dacd
masaru514 <415urasam@gmail.com>
2021-08-02 23:31:11 +0900

    update: show post

--- src/component/organisms/post/index.tsx
+++ src/component/organisms/post/index.tsx
@@ -3,8 +3,13 @@ import React, { ChangeEvent, FormEvent, useState } from 'react'
 
 const Post: React.FC = () => {
   const [postValue, setPostValue] = useState({ text: '' })
+  const [posted, setPosted] = useState<string[]>([])
   const handleSubmit = () => (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    // 投稿されたリストの管理
+    setPosted([...posted, postValue.text])
+
+    // リセット処理
     setPostValue({
       ...postValue,
       text: '',
@@ -21,19 +26,28 @@ const Post: React.FC = () => {
   }
   return (
     <Box display="grid" onSubmit={handleSubmit()}>
-      投稿する
-      <form>
-        <TextField
-          id="filled-multiline-flexible"
-          label="Multiline"
-          multiline
-          maxRows={4}
-          value={postValue.text}
-          onChange={handleChange()}
-          variant="filled"
-        />
-        <Button type="submit">送信</Button>
-      </form>
+      <Box>
+        投稿する
+        <form>
+          <TextField
+            id="filled-multiline-flexible"
+            label="Multiline"
+            multiline
+            maxRows={4}
+            value={postValue.text}
+            onChange={handleChange()}
+            variant="filled"
+          />
+          <Button type="submit">送信</Button>
+        </form>
+      </Box>
+      <Box>
+        <ul>
+          {posted.map((item) => {
+            return <li>{item}</li>
+          })}
+        </ul>
+      </Box>
     </Box>
   )
 }
