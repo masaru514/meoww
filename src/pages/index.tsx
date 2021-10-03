20389f58cbcface867beb7cb4b0168aa3a804a61
masaru514 <415urasam@gmail.com>
2021-10-03 23:29:58 +0900

    redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useContext } from 'react'
 import Contents from 'src/component/molecules/Contents'
+import { AuthContext } from 'src/context/auth'
 
 const HomePage: React.FC = () => {
+  const router = useRouter()
+  const { isAuthChecked } = useContext(AuthContext)
+  if (isAuthChecked) {
+    router.push('/mypage')
+  }
   return (
     <>
       <Contents>
